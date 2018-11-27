a39b37b908d99048a4dbe6ee93ffd67e37d5f21c
DO YUNG YOON <steamshon@apache.org>
2018-11-28 00:08:33 +0900

    add mutateEdge route.  - need to add test cases.

--- s2http/src/main/scala/org/apache/s2graph/http/S2GraphMutateRoute.scala
+++ s2http/src/main/scala/org/apache/s2graph/http/S2GraphMutateRoute.scala
@@ -4,12 +4,13 @@ import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse, StatusC
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.{ExceptionHandler, Route}
 import com.fasterxml.jackson.core.JsonParseException
-import org.apache.s2graph.core.S2Graph
 import org.apache.s2graph.core.rest.RequestParser
+import org.apache.s2graph.core.storage.MutateResponse
+import org.apache.s2graph.core.{GraphElement, S2Graph}
 import org.slf4j.LoggerFactory
 import play.api.libs.json.{JsValue, Json}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 object S2GraphMutateRoute {
 
@@ -21,7 +22,7 @@ trait S2GraphMutateRoute {
   val logger = LoggerFactory.getLogger(this.getClass)
   lazy val parser = new RequestParser(s2graph)
 
-//  lazy val requestParser = new RequestParser(s2graph)
+  //  lazy val requestParser = new RequestParser(s2graph)
   lazy val exceptionHandler = ExceptionHandler {
     case ex: JsonParseException =>
       complete(StatusCodes.BadRequest -> ex.getMessage)
@@ -42,7 +43,7 @@ trait S2GraphMutateRoute {
 
       implicit val ec = s2graph.ec
 
-      val future = tryMutates(payload, operation, serviceNameOpt, columnNameOpt).map { mutateResponses =>
+      val future = vertexMutate(payload, operation, serviceNameOpt, columnNameOpt).map { mutateResponses =>
         HttpResponse(
           status = StatusCodes.OK,
           entity = HttpEntity(ContentTypes.`application/json`, Json.toJson(mutateResponses).toString)
@@ -53,16 +54,56 @@ trait S2GraphMutateRoute {
     }
   }
 
-  def tryMutates(jsValue: JsValue,
-                 operation: String,
-                 serviceNameOpt: Option[String] = None,
-                 columnNameOpt: Option[String] = None,
-                 withWait: Boolean = true)(implicit ec: ExecutionContext) = {
+  lazy val mutateEdge = path("edge" / Segment) { operation =>
+    entity(as[String]) { body =>
+      val payload = Json.parse(body)
+
+      implicit val ec = s2graph.ec
+
+      val future = edgeMutate(payload, operation, withWait = true).map { mutateResponses =>
+        HttpResponse(
+          status = StatusCodes.OK,
+          entity = HttpEntity(ContentTypes.`application/json`, Json.toJson(mutateResponses).toString)
+        )
+      }
+
+      complete(future)
+    }
+  }
+
+  def vertexMutate(jsValue: JsValue,
+                   operation: String,
+                   serviceNameOpt: Option[String] = None,
+                   columnNameOpt: Option[String] = None,
+                   withWait: Boolean = true)(implicit ec: ExecutionContext): Future[Seq[Boolean]] = {
     val vertices = parser.toVertices(jsValue, operation, serviceNameOpt, columnNameOpt)
 
     val verticesToStore = vertices.filterNot(_.isAsync)
 
     s2graph.mutateVertices(verticesToStore, withWait).map(_.map(_.isSuccess))
+  }
+
+  def edgeMutate(elementsWithTsv: Seq[(GraphElement, String)],
+                 withWait: Boolean)(implicit ec: ExecutionContext): Future[Seq[Boolean]] = {
+    val elementWithIdxs = elementsWithTsv.zipWithIndex
+
+    val (elementSync, elementAsync) = elementWithIdxs.partition { case ((element, tsv), idx) =>
+      !element.isAsync
+    }
+    val retToSkip = elementAsync.map(_._2 -> MutateResponse.Success)
+    val (elementsToStore, _) = elementSync.map(_._1).unzip
+    val elementsIdxToStore = elementSync.map(_._2)
+
+    s2graph.mutateElements(elementsToStore, withWait).map { mutateResponses =>
+      elementsIdxToStore.zip(mutateResponses) ++ retToSkip
+    }.map(_.sortBy(_._1).map(_._2.isSuccess))
+  }
+
+  def edgeMutate(jsValue: JsValue,
+                 operation: String,
+                 withWait: Boolean)(implicit ec: ExecutionContext): Future[Seq[Boolean]] = {
+    val edgesWithTsv = parser.parseJsonFormat(jsValue, operation)
+    edgeMutate(edgesWithTsv, withWait)
   }
 
   // expose routes
@@ -71,6 +112,9 @@ trait S2GraphMutateRoute {
       concat(
         handleExceptions(exceptionHandler) {
           mutateVertex
+        },
+        handleExceptions(exceptionHandler) {
+          mutateEdge
         }
       )
     }
